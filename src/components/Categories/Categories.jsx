1546f86040ae1f2d5111ba5a9e6837f26b0030bc
welcomepalygame <welcomeplaygame@gmail.com>
2023-09-21 12:09:03 +0300

    21.09.2023

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { getAllProducts, refreshToken } from "../../api";
 import { ListProduct } from "./ListProduct";
 import InfiniteScroll from "react-infinite-scroll-component";
@@ -6,14 +6,18 @@ import { Preloader } from "../Preloader";
 import { CategorySelect } from "../admin/post/CategorySelect";
 
 export const Categories = () => {
-  const [products, setProducts] = useState([]);
+  const [originalProducts, setOriginalProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
   const [visibleProducts, setVisibleProducts] = useState(6);
   const [minMoney, setMinMoney] = useState(300);
   const [maxMoney, setMaxMoney] = useState();
   const [title, setTitle] = useState("");
   const [category, setCategory] = useState("");
+  const [test, setTest] = useState("");
   const [isFilter, setIsFilter] = useState(true);
   const [filterName, setFilterName] = useState("Відкрити фільтр");
+  const [isLoading, setIsLoading] = useState(false);
+
   const saveData = (key, data) => {
     localStorage.setItem(key, JSON.stringify(data));
   };
@@ -31,19 +35,50 @@ export const Categories = () => {
   useEffect(() => {
     const savedProduct = JSON.parse(localStorage.getItem("products"));
     if (savedProduct) {
-      setProducts(savedProduct);
+      setOriginalProducts(savedProduct);
       console.log("Load from localStorage");
     } else {
       refreshToken()
         .then(() => getAllProducts(sessionStorage.getItem("authToken")))
         .then((data) => {
-          setProducts(data);
+          setOriginalProducts(data);
           saveData("products", data);
           console.log("load from backend");
         })
         .catch((error) => console.error(error));
     }
   }, []);
+
+  useEffect(() => {
+    // Фильтруем оригинальные продукты при изменении критериев фильтрации
+    setIsLoading(true);
+
+    let filteredData = [...originalProducts];
+
+    if (title) {
+      const lowercaseTitle = title.toLowerCase();
+      filteredData = filteredData.filter((product) =>
+        product.title.toLowerCase().includes(lowercaseTitle)
+      );
+    }
+
+    setIsLoading(false);
+    setFilteredProducts(filteredData);
+    saveData("filter", filteredData);
+  }, [originalProducts, title]);
+
+  const clearFilter = () => {
+    setTitle("");
+    setCategory("");
+    setMinMoney(0);
+    setMaxMoney(0);
+
+    const savedProduct = JSON.parse(localStorage.getItem("products"));
+    if (savedProduct) {
+      setFilteredProducts(savedProduct);
+      console.log("Original products");
+    }
+  };
 
   const loading = () => {
     setVisibleProducts((pre) => pre + 3);
@@ -53,7 +88,7 @@ export const Categories = () => {
     <InfiniteScroll
       dataLength={visibleProducts}
       next={loading}
-      hasMore={visibleProducts < products.length}
+      hasMore={visibleProducts < filteredProducts.length}
       loader={<Preloader />}
       scrollThreshold="100px"
       onScroll={null}
@@ -64,6 +99,10 @@ export const Categories = () => {
           className={`all_product_filter ${
             isFilter ? "all_product_filter_no_active" : ""
           }`}
+          // onSubmit={(e) => {
+          //   e.preventDefault();
+          //   applyFilter();
+          // }}
         >
           <div className="all_product_filter_inputs">
             <div className="all_product_filter_box">
@@ -96,8 +135,8 @@ export const Categories = () => {
               </label>
               <input
                 type="number"
-                value={maxMoney}
-                onChange={(e) => setMaxMoney(e.target.value)}
+                value={test}
+                onChange={(e) => setTest(e.target.value)}
                 id="test"
                 className="all_product_filter_input"
               />
@@ -134,9 +173,10 @@ export const Categories = () => {
           }`}
         >
           <button onClick={handleFilter}>{filterName}</button>
+          <button onClick={clearFilter}>Clear</button>
         </div>
         <div className={`all_product ${isFilter ? "all_product_no" : ""}`}>
-          {products.slice(0, visibleProducts).map((product) => (
+          {filteredProducts.slice(0, visibleProducts).map((product) => (
             <ListProduct key={product.id} product={product} />
           ))}
         </div>
